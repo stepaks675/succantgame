3612643ab857eb83db885240a83a64d103ff30b8
stepaks675 <catman775to@mail.ru>
2025-03-13 15:31:09 +0300

    fix

--- succant/src/App.jsx
+++ succant/src/App.jsx
@@ -64,10 +64,10 @@ function App() {
       {gotProved && (
         <VerifierComponent
           a={
-            "11b6a09d2c97600399d69fa4c43ad2ad5d3d0c66c322abf36f0072f0c4331da05db7268f255c0a618e39f2d3e8a19545d008f5a81bc0454580514bec82a7f269b1acbc0f0e5b189bf523c2983231ca5bdf8cb01f826537f0c906e66054b3e2c21c2b73491ca46bd572b1254b1a12bbd9f4fd8003a6043fd9fc03d3289516fecb2fe26ee00768f8127427a7e1453d9ec482ee4fe0f257500497aeb802fb5595fbf825d6390161cee1529a49e652cf7387a74ac6cee625dd34c4a2e6c7321aca1b8010a20725fa4eb46c9ad74507cac2191c4e8b8b6be927a049d22f35327401d43d6caff61d095fcf2ad1208408da55e70678cd199b503e75cc3f0c8b4e37ee70f1638016"
+            proof.proof
           }
-          b={"690000000000000001"}
-          c={"0x00a10aeb968dddf88a30c44a3b130585833591fd35a3b0cd26e1843f3ecc5757"}
+          b={proof.public_values}
+          c={proof.vkey_hash}
         />
       )}
     </div>
